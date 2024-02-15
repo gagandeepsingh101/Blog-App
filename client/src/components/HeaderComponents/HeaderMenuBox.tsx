b9732fabc5a33ca2cec86e0029eab40ec1cd6a29
gagandeepsingh101 <gagandeepsingh128548@gmail.com>
2024-02-15 21:17:50 +0530

    change user profile image size in header

--- client/src/components/HeaderComponents/HeaderMenuBox.tsx
+++ client/src/components/HeaderComponents/HeaderMenuBox.tsx
@@ -27,12 +27,12 @@ const HeaderMenuBox = ({ isOpen, setIsOpen }: {
                 <FaBookmark className='h-5/6  w-2/3 my-auto md:w-1/2' /> <sup className='font-bold absolute top-0 md:text-lg md:-top-2  -right-1'>{bookMarkData.length}</sup>
             </div>
             {/* Button to toggle menu */}
-            <button onClick={() => setIsOpen(!isOpen)} className='rounded-full  text-blue-500 text-center hover:ring-2 hover:ring-blue-500 transition-all duration-300 ease-in-out  h-8 w-10  lg:w-12'>
+            <button onClick={() => setIsOpen(!isOpen)} className='rounded-full  text-blue-500 text-center hover:ring-2 hover:ring-blue-500 transition-all duration-300 ease-in-out  h-8 w-10 md:w-10 md:h-10  lg:w-11 lg:h-11'>
                 <img src={image} className='rounded-full h-full w-full ' alt="" />
             </button>
             {/* Menu content */}
             {isOpen && (
-                <div className=' w-1/2 h-48 right-0 top-14 bg-white absolute  border-2 shadow-md flex flex-col justify-evenly rounded-md   z-50 p-2 md:w-1/4 lg:h-64 lg:w-1/6 lg:top-[70px] lg:right-16'>
+                <div className=' w-1/2 h-48 right-0 top-14 bg-white absolute  border-2 shadow-md flex flex-col justify-evenly rounded-md   z-50 p-2 md:right-2 md:w-1/4 lg:h-64 lg:w-1/6 lg:top-[70px] lg:right-16'>
                     {/* Profile link */}
                     <p onClick={() => {
                         navigate("/profile");
